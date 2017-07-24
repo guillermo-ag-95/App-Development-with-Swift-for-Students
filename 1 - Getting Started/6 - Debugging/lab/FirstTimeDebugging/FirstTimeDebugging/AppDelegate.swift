11235cb176d9c25a2528fd3dea48762418846fa3
Guillermo Alcalá Gamero <guillermo.ag.95@gmail.com>
2017-07-24 19:42:51 +0200

    Lesson 1.6: Building, Running, Debugging an App completed

--- 1 - Getting Started/6 - Debugging/lab/FirstTimeDebugging/FirstTimeDebugging/AppDelegate.swift
+++ 1 - Getting Started/6 - Debugging/lab/FirstTimeDebugging/FirstTimeDebugging/AppDelegate.swift
@@ -20,7 +20,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Override point for customization after application launch.
         var testArray: [String] = ["Uh oh..."]
         testArray.removeFirst()
-        testArray.removeFirst()
         return true
     }
 
