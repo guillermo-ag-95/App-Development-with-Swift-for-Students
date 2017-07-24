fe93c60e69db31c4d59054d45f0f5340abc2a744
Guillermo Alcalá Gamero <guillermo.ag.95@gmail.com>
2017-07-24 19:29:16 +0200

    Lesson 1.5: Xcode completed

---
+++ 1 - Getting Started/5 - Xcode/GettingStarted/GettingStarted/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GettingStarted
+//
+//  Created by Guillermo Alcalá Gamero on 24/7/17.
+//  Copyright © 2017 Guillermo Alcalá Gamero. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
