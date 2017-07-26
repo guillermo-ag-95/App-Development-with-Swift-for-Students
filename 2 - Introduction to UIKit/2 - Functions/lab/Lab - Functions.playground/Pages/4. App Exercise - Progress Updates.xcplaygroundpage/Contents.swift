d76f15fac3c55c694820d29330c87923d2aab883
Guillermo Alcalá Gamero <guillermo.ag.95@gmail.com>
2017-07-26 19:18:31 +0200

    Lesson 2.2: Functions completed

--- 2 - Introduction to UIKit/2 - Functions/lab/Lab - Functions.playground/Pages/4. App Exercise - Progress Updates.xcplaygroundpage/Contents.swift
+++ 2 - Introduction to UIKit/2 - Functions/lab/Lab - Functions.playground/Pages/4. App Exercise - Progress Updates.xcplaygroundpage/Contents.swift
@@ -9,11 +9,36 @@
  
  Call the function a number of times, passing in different values of `steps` and `goal`. Observe the printouts and make sure what is printed to the console is what you would expect for the parameters passsed in.
  */
+func progressUpdate(steps: Int, goal: Int) {
+    if Double(steps) < Double(goal)/10 {
+        print("You're off to a good start.")
+    } else if Double(steps) < Double(goal)/2 {
+        print("You're almost halfway there!")
+    } else if Double(steps) < Double(goal) - Double(goal)/10 {
+        print("You're almost there!")
+    } else if Double(steps) < Double(goal) {
+        print("You beat your goal!")
+    } else {
+        print("Good luck!")
+    }
+}
 
-
+progressUpdate(steps: 5400, goal: 10000)
+progressUpdate(steps: 230, goal: 15000)
+progressUpdate(steps: 9000, goal: 10000)
 /*:
  Your fitness tracking app is going to help runners stay on pace to reach their goals. Write a function called pacing that takes four `Double` parameters called `currentDistance`, `totalDistance`, `currentTime`, and `goalTime`. Your function should calculate whether or not the user is on pace to hit or beat `goalTime`. If yes, print "Keep it up!", otherwise print "You've got to push it just a bit harder!"
  */
+func pacing(currentDistance: Double, totalDistance: Double, currentTime: Double, goalTime: Double) {
+    let currentSpeed: Double = currentDistance / currentTime
+    let goalSpeed: Double = totalDistance / goalTime
+    
+    if currentSpeed > goalSpeed {
+        print("Keep it up!")
+    } else {
+        print("You've got to push it just a bit harder!")
+    }
+}
 
-
+pacing(currentDistance: 200, totalDistance: 1000, currentTime: 2, goalTime: 12)
 //: [Previous](@previous)  |  page 4 of 6  |  [Next: Exercise - Return Values](@next)
