99588ead6e325da6ee8e8ffe7d3b4a791594d0e5
Guillermo Alcalá Gamero <guillermo.ag.95@gmail.com>
2017-07-28 16:51:33 +0200

    Lesson 2.6: Loops completed

--- 2 - Introduction to UIKit/6 - Loops/lab/Lab - Loops.playground/Pages/3. Exercise - While Loops.xcplaygroundpage/Contents.swift
+++ 2 - Introduction to UIKit/6 - Loops/lab/Lab - Loops.playground/Pages/3. Exercise - While Loops.xcplaygroundpage/Contents.swift
@@ -4,7 +4,10 @@ import Foundation
  
  Create a while loop that simulates rolling a 6-sided dice repeatedly until a 1 is rolled. After each roll, print the value. (Hint: use `Int(arc4random_uniform(6) + 1)` to generate a random number between 1 and 6).
  */
+var dice: Int = 0
 
-
-
+while dice != 1 {
+    dice = Int(arc4random_uniform(6) + 1)
+    print(dice)
+}
 //: [Previous](@previous)  |  page 3 of 6  |  [Next: App Exercise - While Loops](@next)
--- 2 - Introduction to UIKit/6 - Loops/lab/Lab - Loops.playground/Pages/4. App Exercise - Running Cadence.xcplaygroundpage/Contents.swift
+++ 2 - Introduction to UIKit/6 - Loops/lab/Lab - Loops.playground/Pages/4. App Exercise - Running Cadence.xcplaygroundpage/Contents.swift
@@ -9,13 +9,21 @@ import Foundation
  For this exercise, you'll simulate a "test run" of the cadence feature of your app. Use a while loop to print "Take a step" to the console 10 times. Once you've successfully printed "Take a step" to the console 10 times, add the following code to the end of your while loop: `Thread.sleep(forTimeInterval: 60/cadence)`. This will put a pause between each itertion of the while loop so that the print statements actually occur at the proper cadence.
  */
 let cadence: Double = 180
-var testSteps = 0
+var testSteps = 10
 
-
+while testSteps != 0 {
+    testSteps -= 1
+    print("Take a step")
+    Thread.sleep(forTimeInterval: 60/cadence)
+}
 /*:
  Recreate the above cadence example using a repeat-while loop.
  */
-testSteps = 0 
+testSteps = 10
 
-
+repeat {
+    testSteps -= 1
+    print("Take a step")
+    Thread.sleep(forTimeInterval: 60/cadence)
+} while testSteps != 0
 //: [Previous](@previous)  |  page 4 of 6  |  [Next: Exercise - Control Transfer Statements](@next)
