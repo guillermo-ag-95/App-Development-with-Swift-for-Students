743f16dd260892d7709774b5cd5679c0570f9b87
Guillermo Alcalá Gamero <guillermo.ag.95@gmail.com>
2017-08-01 20:37:59 +0200

    Lesson 3.3: Guard completed

--- 3 - Navigation and Workflows/3 - Guard/lab/Lab - Guard.playground/Pages/2. App Exercise - Guard.xcplaygroundpage/Contents.swift
+++ 3 - Navigation and Workflows/3 - Guard/lab/Lab - Guard.playground/Pages/2. App Exercise - Guard.xcplaygroundpage/Contents.swift
@@ -10,8 +10,19 @@ import UIKit
  
  Write a failable initializer that takes parameters for your start and end times, and then checks to see if they are greater than 10 seconds apart using a guard statement. If they are, your initializer should fail. Otherwise, the initializer should set the properties accordingly.
  */
+struct Workout {
+    var startTime: Double
+    var endTime: Double
+    
+    init?(startTime: Double, endTime: Double) {
+        guard endTime - startTime > 10 else { return nil }
+        self.startTime = startTime
+        self.endTime = endTime
+    }
+}
 
-
+Workout.init(startTime: 28_800, endTime: 30_000)
+Workout.init(startTime: 28_800, endTime: 28_801)
 /*:
  Imagine a screen where a user inputs a meal that they've eaten. If the user taps a "save" button without adding any food, you might want to prompt the user that they haven't actually added anything.
  
@@ -28,12 +39,17 @@ let caloriesTextField = UITextField()
 foodTextField.text = "Banana"
 caloriesTextField.text = "23"
 
-
+func logFood() -> Food? {
+    guard let food = foodTextField.text, let calories = Int(caloriesTextField.text!) else { return nil }
+    return Food.init(name: food, calories: calories)
+}
 /*:
  Call the function you made above and capture the return value. Unwrap the `Food` object with standard optional binding and print a statement about the food using each of its properties. Go back and change the text in `caloriesTextField` to a string that cannot be converted into a number. What happens in that case?
  */
-
-
+var newFood: Food? = logFood()
+if let food = newFood {
+    print("Food name: \(food.name), Calories: \(food.calories)")
+}
 /*:
  
  _Copyright © 2017 Apple Inc._
