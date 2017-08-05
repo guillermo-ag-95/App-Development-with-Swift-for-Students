cf5447c24703e6c94dd3d5be988a89afbd70db89
Guillermo Alcalá Gamero <guillermo.ag.95@gmail.com>
2017-08-05 12:20:57 +0200

    Lesson 3.7: Tab Bar Controllers completed (theory)

---
+++ 3 - Navigation and Workflows/7 - Tab Bar Controllers/RainbowTabs/RainbowTabs/RedViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  RainbowTabs
+//
+//  Created by Guillermo Alcalá Gamero on 5/8/17.
+//  Copyright © 2017 Guillermo Alcalá Gamero. All rights reserved.
+//
+
+import UIKit
+
+class RedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
