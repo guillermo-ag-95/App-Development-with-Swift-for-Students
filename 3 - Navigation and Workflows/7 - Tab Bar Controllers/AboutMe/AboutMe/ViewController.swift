96cb88aab43d69925d7851954916390f32a4f518
Guillermo Alcalá Gamero <guillermo.ag.95@gmail.com>
2017-08-05 13:07:35 +0200

    Lesson 3.7: Tab Bar Controllers completed (lab)

---
+++ 3 - Navigation and Workflows/7 - Tab Bar Controllers/AboutMe/AboutMe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AboutMe
+//
+//  Created by Guillermo Alcalá Gamero on 5/8/17.
+//  Copyright © 2017 Guillermo Alcalá Gamero. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
