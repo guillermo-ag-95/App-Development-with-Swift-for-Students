d5beac4d9ed77a73b780a0128537ae22dc38a2eb
Guillermo Alcalá Gamero <guillermo.ag.95@gmail.com>
2017-08-07 14:23:17 +0200

    Lesson 4.1: Protocols completed

--- 4 - Tables and Persistence/1 - Protocols/lab/Lab - Protocols.playground/Pages/1. Exercise - Adopt Protocols.xcplaygroundpage/Contents.swift
+++ 4 - Tables and Persistence/1 - Protocols/lab/Lab - Protocols.playground/Pages/1. Exercise - Adopt Protocols.xcplaygroundpage/Contents.swift
@@ -3,21 +3,48 @@
  
  Create a `Human` class with two properties: `name` of type `String`, and `age` of type `Int`. You'll need to create a memberwise initializer for the class. Initialize two `Human` instances.
  */
+class Human: CustomStringConvertible, Equatable, Comparable {
+    var name: String
+    var age: Int
+    
+    init(name: String, age: Int) {
+        self.name = name
+        self.age = age
+    }
+    
+    var description: String {
+        return "Human(name: \(name), age: \(age))"
+    }
+    
+    static func ==(lhs: Human, rhs: Human) -> Bool {
+        return lhs.name == rhs.name && lhs.age == rhs.age
+    }
+    
+    static func <(lhs: Human, rhs: Human) -> Bool {
+        return lhs.age < rhs.age
+    }
 
+}
 
+let human1: Human = Human(name: "Guillermo", age: 21)
+let human2: Human = Human(name: "Javier", age: 54)
 /*:
  Make the `Human` class adopt the `CustomStringConvertible`. Print both of your previously initialized `Human` objects.
  */
-
-
+print(human1)
+print(human2)
 /*:
  Make the `Human` class adopt the `Equatable` protocol. Two instances of `Human` should be considered equal if their names and ages are identical to one another. Print the result of a boolean expression evaluating whether or not your two previously initialized `Human` objects are equal to eachother (using `==`). Then print the result of a boolean expression evaluating whether or not your two previously initialized `Human` objects are not equal to eachother (using `!=`).
  */
-
-
+print(human1 == human2)
+print(human1 != human2)
 /*:
  Make the `Human` class adopt the `Comparable` protocol. Sorting should be based on age. Create another three instances of a `Human`, then create an array called `people` of type `[Human]` with all of the `Human` objects that you have initialized. Create a new array called `sortedPeople` of type `[Human]` that is the `people` array sorted by age.
  */
+let human3: Human = Human(name: "Luisa", age: 52)
+let human4: Human = Human(name: "Andy", age: 22)
+let human5: Human = Human(name: "Ale", age: 22)
 
-
+let people: [Human] = [human1, human2, human3, human4, human5]
+let sortedPeople: [Human] = people.sorted(by: <)
 //: page 1 of 5  |  [Next: App Exercise - Printable Workouts](@next)
--- 4 - Tables and Persistence/1 - Protocols/lab/Lab - Protocols.playground/Pages/2. App Exercise - Printable Workouts.xcplaygroundpage/Contents.swift
+++ 4 - Tables and Persistence/1 - Protocols/lab/Lab - Protocols.playground/Pages/2. App Exercise - Printable Workouts.xcplaygroundpage/Contents.swift
@@ -5,33 +5,50 @@
  
  The `Workout` objects you have created so far in app exercises don't show a whole lot of useful information when printed to the console. They also aren't very easy to compare or sort. Throughout these exercises, you'll make the `Workout` class below adopt certain protocols that will solve these issues.
  */
-class Workout {
+class Workout: CustomStringConvertible, Equatable, Comparable {
     var distance: Double
     var time: Double
     var identifier: Int
+    
+    var description: String {
+        return "Workout(distance: \(distance), time: \(time), identifier: \(identifier))"
+    }
     
     init(distance: Double, time: Double, identifier: Int) {
         self.distance = distance
         self.time = time
         self.identifier = identifier
     }
+    
+    static func ==(lhs: Workout, rhs: Workout) -> Bool {
+        return lhs.identifier == rhs.identifier
+    }
+    
+    static func <(lhs: Workout, rhs: Workout) -> Bool {
+        return lhs.identifier < rhs.identifier
+    }
+    
 }
 
 /*:
  Make the `Workout` class above conform to the `CustomStringConvertible` protocol so that printing an instance of `Workout` will provide useful information in the console. Create an instance of `Workout`, give it an identifier of 1, and print it to the console.
  */
-
-
+let workout1: Workout = Workout(distance: 1500, time: 8, identifier: 1)
+print(workout1)
 /*:
  Make the `Workout` class above conform to the `Equatable` protocol. Two `Workout` objects should be considered equal if they have the same identifier. Create another instance of `Workout`, giving it an identifier of 2, and print a boolean expression that evaluates to whether or not it is equal to the first `Workout` instance you created.
  */
-
-
+let workout2: Workout = Workout(distance: 3000, time: 19, identifier: 2)
+print(workout1 == workout2)
 /*:
  Make the `Workout` class above conform to the `Comparable` protocol so that you can easily sort multiple instances of `Workout`. `Workout` objects should be sorted based on their identifier. 
  
  Create three more `Workout` objects, giving them identifiers of 3, 4, and 5, respectively. Then create an array called `workouts` of type `[Workout]` and assign it an array literal with all five `Workout` objects you have created. Place these objects in the array out of order. Then create another array called `sortedWorkouts` of type `[Workout]` that is the `workouts` array sorted by identifier. 
  */
+let workout3: Workout = Workout(distance: 2500, time: 16, identifier: 3)
+let workout4: Workout = Workout(distance: 500, time: 2, identifier: 4)
+let workout5: Workout = Workout(distance: 10000, time: 40, identifier: 5)
 
-
+let workouts: [Workout] = [workout1, workout5, workout2, workout4, workout3]
+let sortedWorkots: [Workout] = workouts.sorted(by: <)
 //: [Previous](@previous)  |  page 2 of 5  |  [Next: Exercise - Create a Protocol](@next)
