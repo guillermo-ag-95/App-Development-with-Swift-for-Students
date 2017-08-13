50917f32755de838d528baf170ba58e3af49d900
Guillermo Alcalá Gamero <guillermo.ag.95@gmail.com>
2017-08-13 17:53:09 +0200

    Lesson 4.7: Saving Data completed (lab)

--- 4 - Tables and Persistence/7 - Saving Data/lab/EmojiDictionary/EmojiDictionary/EmojiTableViewController.swift
+++ 4 - Tables and Persistence/7 - Saving Data/lab/EmojiDictionary/EmojiDictionary/EmojiTableViewController.swift
@@ -3,19 +3,7 @@ import UIKit
 
 class EmojiTableViewController: UITableViewController {
     
-    var emojis = [Emoji(symbol: "😀", name: "Grinning Face", detailDescription: "A typical smiley face.", usage: "happiness"),
-                  Emoji(symbol: "😕", name: "Confused Face", detailDescription: "A confused, puzzled face.", usage: "unsure what to think; displeasure"),
-                  Emoji(symbol: "😍", name: "Heart Eyes", detailDescription: "A smiley face with hearts for eyes.", usage: "love of something; attractive"),
-                  Emoji(symbol: "👮", name: "Police Officer", detailDescription: "A police officer wearing a blue cap with a gold badge. He is smiling, and eager to help.", usage: "person of authority"),
-                  Emoji(symbol: "🐢", name: "Turtle", detailDescription: "A cute turtle.", usage: "Something slow"),
-                  Emoji(symbol: "🐘", name: "Elephant", detailDescription: "A gray elephant.", usage: "good memory"),
-                  Emoji(symbol: "🍝", name: "Spaghetti", detailDescription: "A plate of spaghetti.", usage: "spaghetti"),
-                  Emoji(symbol: "🎲", name: "Die", detailDescription: "A single die.", usage: "taking a risk, chance; game"),
-                  Emoji(symbol: "⛺️", name: "Tent", detailDescription: "A small tent.", usage: "camping"),
-                  Emoji(symbol: "📚", name: "Stack of Books", detailDescription: "Three colored books stacked on each other.", usage: "homework, studying"),
-                  Emoji(symbol: "💔", name: "Broken Heart", detailDescription: "A red, broken heart.", usage: "extreme sadness"),
-                  Emoji(symbol: "💤", name: "Snore", detailDescription: "Three blue \'z\'s.", usage: "tired, sleepiness"),
-                  Emoji(symbol: "🏁", name: "Checkered Flag", detailDescription: "A black and white checkered flag.", usage: "completion")]
+    var emojis: [Emoji] = []
 
     // MARK: - Table view data source
 
@@ -55,6 +43,8 @@ class EmojiTableViewController: UITableViewController {
         if editingStyle == .delete {
             emojis.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .automatic)
+            // Added
+            Emoji.saveToFile(emojis: emojis)
         }
     }
     
@@ -62,6 +52,8 @@ class EmojiTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
         let movedEmoji = emojis.remove(at: fromIndexPath.row)
         emojis.insert(movedEmoji, at: to.row)
+        // Added
+        Emoji.saveToFile(emojis: emojis)
         tableView.reloadData()
     }
     
@@ -78,6 +70,13 @@ class EmojiTableViewController: UITableViewController {
         
         self.refreshControl = UIRefreshControl()
         self.refreshControl?.addTarget(self, action: #selector(refreshControlActivated(sender:)), for: .valueChanged)
+        
+        if let savedData = Emoji.loadFromFile() {
+            emojis.append(contentsOf: savedData)
+        } else {
+            emojis.append(contentsOf: Emoji.loadSampleEmojis())
+        }
+        
     }
 
     
@@ -95,6 +94,8 @@ class EmojiTableViewController: UITableViewController {
                 emojis.append(emoji)
                 tableView.insertRows(at: [newIndexPath], with: .automatic)
             }
+            // Added
+            Emoji.saveToFile(emojis: emojis)
         }
     }
     
