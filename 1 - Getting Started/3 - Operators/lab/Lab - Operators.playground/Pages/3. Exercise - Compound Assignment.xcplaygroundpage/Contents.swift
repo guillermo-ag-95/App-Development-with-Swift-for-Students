c5e35cd601c1c72302f00c378a5742f81bf1414c
Guillermo <guillermo.ag.95@gmail.com>
2018-12-14 13:39:37 +0100

    Lesson 1.3: Operators completed

--- 1 - Getting Started/3 - Operators/lab/Lab - Operators.playground/Pages/3. Exercise - Compound Assignment.xcplaygroundpage/Contents.swift
+++ 1 - Getting Started/3 - Operators/lab/Lab - Operators.playground/Pages/3. Exercise - Compound Assignment.xcplaygroundpage/Contents.swift
@@ -3,8 +3,12 @@
  
  Declare a variable whose value begins at 10. Using addition, update the value to 15 using the compound assignment operator. Using multiplication, update the value to 30 using compound assignment. Print out the variable's value after each assignment.
  */
-
-
+var variable = 10
+print(variable)
+variable += 5
+print(variable)
+variable *= 2
+print(variable)
 /*:
  Create a variable called `piggyBank` that begins at 0. You will use this to keep track of money you earn and spend. For each point below, use the right compound assignment operator to update the balance in your piggy bank.
  
@@ -16,9 +20,16 @@
  
  Print the balance of your piggy bank after each step.
  */
-
-
-
-
-
+var piggyBack = 0
+print(piggyBack)
+piggyBack += 10
+print(piggyBack)
+piggyBack += 20
+print(piggyBack)
+piggyBack /= 2
+print(piggyBack)
+piggyBack *= 3
+print(piggyBack)
+piggyBack += 3
+print(piggyBack)
 //: [Previous](@previous)  |  page 3 of 8  |  [Next: App Exercise - Counting](@next)
--- 1 - Getting Started/3 - Operators/lab/Lab - Operators.playground/Pages/8. App Exercise - Fitness Conversions.xcplaygroundpage/Contents.swift
+++ 1 - Getting Started/3 - Operators/lab/Lab - Operators.playground/Pages/8. App Exercise - Fitness Conversions.xcplaygroundpage/Contents.swift
@@ -7,8 +7,10 @@
  
  Now create a constant `percentOfGoal` of type `Double` that equals the percent of the goal that has been reached so far. You'll need to convert your constants of type `Int` to be of type `Double` in your calculation.
  */
+var steps = 3_200
+var goal = 10_000
 
-
+let percentOfGoal = Double(steps) / Double(goal)
 /*:
  
  _Copyright © 2017 Apple Inc._
