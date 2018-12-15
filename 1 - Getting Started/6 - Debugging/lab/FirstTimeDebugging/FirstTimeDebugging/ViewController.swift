d517f222ffa22f5d9f33ac71e489d29d1c3498df
Guillermo <guillermo.ag.95@gmail.com>
2018-12-15 13:28:58 +0100

    Lesson 1.6: Building, Running, Debugging an App completed

--- 1 - Getting Started/6 - Debugging/lab/FirstTimeDebugging/FirstTimeDebugging/ViewController.swift
+++ 1 - Getting Started/6 - Debugging/lab/FirstTimeDebugging/FirstTimeDebugging/ViewController.swift
@@ -2,17 +2,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad( {
+    override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        var sample = "sample"
+        let sample = "sample"
         print(sample)
-        
-        if false {
-            print("Will this line of code ever be reached?")
-            someMetho()
-        }
-        
         
     }
     
