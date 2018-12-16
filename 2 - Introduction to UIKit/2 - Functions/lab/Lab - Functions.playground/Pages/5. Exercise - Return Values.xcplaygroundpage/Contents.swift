9ecbd3879b313bab2421ddc431089a3ffd84913e
Guillermo <guillermo.ag.95@gmail.com>
2018-12-16 14:41:18 +0100

    Lesson 2.2: Functions completed

--- 2 - Introduction to UIKit/2 - Functions/lab/Lab - Functions.playground/Pages/5. Exercise - Return Values.xcplaygroundpage/Contents.swift
+++ 2 - Introduction to UIKit/2 - Functions/lab/Lab - Functions.playground/Pages/5. Exercise - Return Values.xcplaygroundpage/Contents.swift
@@ -3,11 +3,17 @@
  
  Write a function called `greeting` that takes a `String` argument called name, and returns a `String` that greets the name that was passed into the function. I.e. if you pass in "Dan" the return value might be "Hi, Dan! How are you?" Use the function and print the result.
  */
+func greeting(name: String) -> String {
+    return "Hi, \(name)! How are you?"
+}
 
-
+print(greeting(name: "Guillermo"))
 /*:
  Write a function that takes two `Int` arguments, and returns an `Int`. The function should multiply the two arguments, add 2, then return the result. Use the function and print the result.
  */
+func multiply(first: Int, second: Int) -> Int {
+    return first * second + 2
+}
 
-
+print(multiply(first: 2, second: 4))
 //: [Previous](@previous)  |  page 5 of 6  |  [Next: App Exercise - Separating Functions](@next)
