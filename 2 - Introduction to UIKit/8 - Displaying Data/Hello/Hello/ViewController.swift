860efa28127df6ce34f41e527fb43e89117adf24
Guillermo <guillermo.ag.95@gmail.com>
2018-12-20 13:42:32 +0100

    Lesson 2.8: Displaying data completed

---
+++ 2 - Introduction to UIKit/8 - Displaying Data/Hello/Hello/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hello
+//
+//  Created by Guillermo Alcalá Gamero on 19/12/2018.
+//  Copyright © 2018 Guillermo Alcalá Gamero. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
