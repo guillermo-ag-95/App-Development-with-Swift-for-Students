512f0ff7899cbb052a8ff4c8fd6315e127c6318f
Guillermo <guillermo.ag.95@gmail.com>
2018-12-20 14:38:17 +0100

    Lesson 2.10: Auto Layout and Stack Views completed

---
+++ 2 - Introduction to UIKit/10 - Auto Layout and Stack Views/lab/Calculator/Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Guillermo Alcalá Gamero on 20/12/2018.
+//  Copyright © 2018 Guillermo Alcalá Gamero. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
