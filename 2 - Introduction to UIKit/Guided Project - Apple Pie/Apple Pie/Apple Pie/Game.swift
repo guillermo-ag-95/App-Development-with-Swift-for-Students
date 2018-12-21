17a2469262e0ab64773b948a0fd4164f8b24635d
Guillermo <guillermo.ag.95@gmail.com>
2018-12-21 14:35:52 +0100

    Lesson 2: Guided Project - Apple Pie completed

---
+++ 2 - Introduction to UIKit/Guided Project - Apple Pie/Apple Pie/Apple Pie/Game.swift
@@ -0,0 +1,33 @@
+//
+//  Game.swift
+//  Apple Pie
+//
+//  Created by Guillermo Alcalá Gamero on 21/12/2018.
+//  Copyright © 2018 Guillermo Alcalá Gamero. All rights reserved.
+//
+
+import Foundation
+
+struct Game {
+    var word: String
+    var incorrectMovesRemaining: Int
+    var guessedLetters: [Character]
+    var formattedWord: String {
+        var guessedWord = ""
+        for letter in word {
+            if guessedLetters.contains(letter) {
+                guessedWord += "\(letter)"
+            } else {
+                guessedWord += "_"
+            }
+        }
+        return guessedWord
+    }
+    
+    mutating func playerGuessed(letter: Character) {
+        guessedLetters.append(letter)
+        if !word.contains(letter) {
+            incorrectMovesRemaining -= 1
+        }
+    }
+}
---
+++ 2 - Introduction to UIKit/Guided Project - Apple Pie/Apple Pie/Apple Pie/ViewController.swift
@@ -0,0 +1,88 @@
+//
+//  ViewController.swift
+//  Apple Pie
+//
+//  Created by Guillermo Alcalá Gamero on 21/12/2018.
+//  Copyright © 2018 Guillermo Alcalá Gamero. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    var listOfWords = ["buccaneer", "swift", "glorious", "incandescent", "bug", "program"]
+    var incorrectMovesAllowed = 7
+    
+    var totalWins = 0 {
+        didSet {
+            newRound()
+        }
+    }
+    var totalLoses = 0 {
+        didSet {
+            newRound()
+        }
+    }
+    
+    var currentGame: Game!
+    
+    @IBOutlet weak var treeImageView: UIImageView!
+    @IBOutlet weak var correctWordLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet var letterButtons: [UIButton]!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        newRound()
+    }
+
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        sender.isEnabled = false
+        let letterString = sender.title(for: .normal)!
+        let letter = Character(letterString.lowercased())
+        currentGame.playerGuessed(letter: letter)
+        updateGameState()
+    }
+    
+    func newRound() {
+        if !listOfWords.isEmpty {
+            let newWord = listOfWords.removeFirst()
+            currentGame = Game(word: newWord, incorrectMovesRemaining: incorrectMovesAllowed, guessedLetters: [])
+            enableLetterButtons(true)
+            updateUI()
+        } else {
+            enableLetterButtons(false)
+        }
+        
+        
+    }
+    
+    func updateUI() {
+        var letters = [String]()
+        for letter in currentGame.formattedWord {
+            letters.append(String(letter))
+        }
+        let wordWithSpacing = letters.joined(separator: " ")
+        correctWordLabel.text = wordWithSpacing
+        scoreLabel.text = "Wins: \(totalWins), Loses: \(totalLoses)"
+        treeImageView.image = UIImage(named: "Tree \(currentGame.incorrectMovesRemaining)")
+    }
+    
+    func updateGameState() {
+        if currentGame.incorrectMovesRemaining == 0 {
+            totalLoses += 1
+        } else if currentGame.word == currentGame.formattedWord {
+            totalWins += 1
+        } else {
+            updateUI()
+        }
+    }
+    
+    func enableLetterButtons(_ enable: Bool) {
+        for button in letterButtons {
+            button.isEnabled = enable
+        }
+    }
+}
+
